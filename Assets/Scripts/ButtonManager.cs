47ae4419b63336f8556750e009086dfb11275fe2
Johndell S. Kitts <k.jaydeee@gmail.com>
2022-06-17 12:42:17 +0800

    Added new canvas that let's you select level, quit can now also exit playmode from editor

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -2,13 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class ButtonManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject levelSelector, mainMenu;
     // reset the scene
     public void ResetScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ChooseLevel()
+    {
+        mainMenu.SetActive(false);
+        levelSelector.SetActive(true);
+    }
+
+    public void ChooseLevelBackToMainMenu()
+    {
+        levelSelector.SetActive(false);
+        mainMenu.SetActive(true);
     }
 
     // load the selected name of scene
@@ -17,9 +32,18 @@ public class ButtonManager : MonoBehaviour
         SceneManager.LoadScene(levelName);
     }
 
+    public void LevelLoadScene(int element)
+    {
+        SceneManager.LoadScene(element);
+    }
+
     // quit the game
     public void QuitGame()
     {
+        # if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+        #else
         Application.Quit();
+        #endif
     }
 }
